0cda589f33d388cdcc4eb6549a71222f1b4cb1c3
Yifei Li <liy12@rose-hulman.edu>
2022-09-11 19:55:19 -0700

    Update YifeiLiScene

---
+++ Assets/Scripts/DarkLightBlockController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DarkLightBlockController : MonoBehaviour
+{
+    public bool isWhite;
+
+    private SpriteRenderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = gameObject.GetComponent<SpriteRenderer>();
+        renderer.color = isWhite ? Color.white : Color.black;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            isWhite = !isWhite;
+        }
+        ChangeColor();
+    }
+
+    void ChangeColor()
+    {
+        renderer.color = isWhite ? Color.white : Color.black;
+    }
+}
