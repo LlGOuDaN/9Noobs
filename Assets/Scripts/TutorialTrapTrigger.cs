303cc677bc18b2f4aa37f80c8aeb17c43615e8a7
leadlost <eason.yuchendai@gmail.com>
2022-09-17 21:01:35 -0700

    Add trap to tutorial2

---
+++ Assets/Scripts/TutorialTrapTrigger.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTrapTrigger : MonoBehaviour
+{
+
+    private GameObject blackStepTrap;
+    private GameObject pressK;
+    // Start is called before the first frame update
+    void Start()
+    {
+        blackStepTrap = GameObject.Find("BlackStepTrap");
+        pressK = GameObject.Find("Press_K");
+        blackStepTrap.SetActive(false);
+        pressK.SetActive(false);
+    }
+
+    void DisplayText()
+    {
+        pressK.SetActive(true);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Trigger enter.");
+        blackStepTrap.SetActive(true);
+        Invoke("DisplayText", 2.0f);
+    }
+}
