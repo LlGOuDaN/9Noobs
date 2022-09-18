2bf71e9a9381939efbf28c46be26a3b5e528e9e5
Yifei Li <liy12@rose-hulman.edu>
2022-09-17 19:44:42 -0700

    fix conflict

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -6,49 +6,39 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMovementController : MonoBehaviour
 {
-    public float movePower = 10f;
-    public float jumpPower = 20f; //Set Gravity Scale in Rigidbody2D Component to 5
+   public float movePower = 10f;
+        public float jumpPower = 20f; //Set Gravity Scale in Rigidbody2D Component to 5
 
-    private Rigidbody2D rb;
-    private Animator anim;
-    Vector3 movement;
-    bool doubleJump;
-    private float horizontalMovement;
+        private Rigidbody2D rb;
+        private Animator anim;
+        Vector3 movement;
+        bool doubleJump;
+        private float horizontalMovement;
 
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private LayerMask deadCheckLayer;
+        [SerializeField] private Transform groundCheck;
+        [SerializeField] private LayerMask groundLayer;
+        [SerializeField] private LayerMask deadCheckLayer;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(groundCheck.position, 0.1f);
-    }
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, 0.1f);
+        }
 
-<<<<<<< main
-   private void Update()
-   {
-       if (!IsDead())
-       {
-           horizontalMovement = Input.GetAxisRaw("Horizontal");
-           Jump();
-       }      
-       else
-       {
-           Invoke(nameof(RestartLevel), 0.5f);
-       }
-   }
-=======
         private void Update()
         {
             if (!IsDead())
             {
+                if (GameManager.disableInput)
+                {
+                    return;
+                }
                 if (IsGrounded() && !doubleJump) 
                 {
                     doubleJump = true;    
@@ -61,72 +51,35 @@ public class PlayerMovementController : MonoBehaviour
                 Invoke(nameof(RestartLevel), 0.5f);
             }
         }
->>>>>>> Update PlayerMovementController.cs
 
-   private void RestartLevel()
-   {
-       SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-   }
+        private void RestartLevel()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
 
-   private void FixedUpdate()
-   {
-       if (!IsDead())
-       {
-           Run();
-       }
-   }
+        private void FixedUpdate()
+        {
+            if (!IsDead() && !GameManager.disableInput)
+            {
+                Run();
+            }
+        }
 
-   private bool IsGrounded()
-   {
-       return Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
-   }
+        private bool IsGrounded()
+        {
+            return Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+        }
 
-   private bool IsDead()
-   {
-       return Physics2D.OverlapCircle(groundCheck.position, 0.2f, deadCheckLayer);
-   }
+        private bool IsDead()
+        {
+            return Physics2D.OverlapCircle(groundCheck.position, 0.2f, deadCheckLayer);
+        }
 
-   void Run()
-   {
-       if (GameManager.disableInput)
-       {
-           return;
-       }
-       Vector3 moveVelocity = Vector3.zero;
-       Vector3 localScale = transform.localScale;
+        void Run()
+        {
+            Vector3 moveVelocity = Vector3.zero;
+            Vector3 localScale = transform.localScale;
 
-<<<<<<< main
-       if (horizontalMovement < 0)
-       {
-           moveVelocity = Vector3.left;
-            
-           if(localScale.x > 0) {
-               localScale.x *= -1f;
-               transform.localScale = localScale;
-           }
-       }
-       if (horizontalMovement > 0)
-       {
-           moveVelocity = Vector3.right;
-           if(localScale.x < 0) {
-               localScale.x *= -1f;
-               transform.localScale = localScale;
-           }
-       }
-       transform.position += moveVelocity * (movePower * Time.fixedDeltaTime);
-   }
-   void Jump()
-   {
-       if ((Input.GetButtonDown("Jump")))
-       {
-           if (IsGrounded() || (Input.GetButtonDown("Jump")))
-           {
-               rb.velocity = new Vector2(rb.velocity.x, jumpPower);
-           }
-       }
-   }
-    }
-=======
             if (horizontalMovement < 0)
             {
                 moveVelocity = Vector3.left;
@@ -158,26 +111,4 @@ public class PlayerMovementController : MonoBehaviour
                 }
             }
         }
-
-    /*      这里修改了下二段跳， 原先的二段跳太难按出来了。   
-        void Jump()
-        {
-            if ((Input.GetButtonDown("Jump")))
-            {
-                if (IsGrounded() || doubleJump)
-                {
-                    if (doubleJump)
-                    {
-                        Debug.Log("double");
-                    }
-                    Debug.Log("jump");
-                    rb.velocity = new Vector2(rb.velocity.x, jumpPower);
-                    doubleJump = !doubleJump;
-                }
-            }
-        }
-
-
-        */
 }
->>>>>>> Update PlayerMovementController.cs
