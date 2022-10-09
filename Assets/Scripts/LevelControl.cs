d7673dbbe33020caeec787ff1de85665cf8aed7b
Qianyr22 <2523768326@qq.com>
2022-10-08 17:54:45 -0700

    yr

---
+++ Assets/Scripts/LevelControl.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelControl : MonoBehaviour
+{
+    public void scene0()
+    {
+        SceneManager.LoadScene("0");
+    }
+
+    public void scene1()
+    {
+        SceneManager.LoadScene("1");
+    }
+
+    public void scene2()
+    {
+        SceneManager.LoadScene("2");
+    }
+
+    public void scene3()
+    {
+        SceneManager.LoadScene("3");
+    }
+
+    public void back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused=false;
+    public GameObject PauseMenuUI;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(GameIsPaused){
+                Resume();
+            }else{
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+        GameIsPaused = true;
+    }
+
+    public void Menu()
+    {
+        GameIsPaused = false;
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
