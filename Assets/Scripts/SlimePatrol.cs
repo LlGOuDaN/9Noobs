38117fa2160dadefd2779d5dec2d002935bc16c5
Zhiheng <dzh2077@gmail.com>
2022-11-17 14:14:04 -0800

    zhiheng: clear slime script

--- Assets/Scripts/SlimePatrol.cs
+++ Assets/Scripts/SlimePatrol.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SlimePatrol : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,7 @@ public class SlimePatrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         
     }
 }
