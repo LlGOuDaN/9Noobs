0a00b83a66326902995ec33c5a7b2358338cea43
Qianyr22 <2523768326@qq.com>
2022-11-22 00:50:15 -0800

    coin

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+    public TextMeshProUGUI text;
+    int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance==null){
+            instance=this;
+        }
+    }
+
+    public void changeScore(int coinValue){
+        score+=coinValue;
+        text.text="X"+score.ToString();
+    }
+}
