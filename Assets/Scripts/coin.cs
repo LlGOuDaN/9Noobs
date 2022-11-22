9fd691182d1969b42a79a9db48705221a6dc7001
Qianyr22 <2523768326@qq.com>
2022-11-22 00:50:15 -0800

    coin

---
+++ Assets/Scripts/coin.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour
+{
+    public int coinValue =1;
+    
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.CompareTag("Player")){
+            ScoreManager.instance.changeScore(coinValue);
+        }
+    }
+}
