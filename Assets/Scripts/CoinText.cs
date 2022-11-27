7d53c3df5fc1c07a641f8e4d72b5919485d1d914
Zhiheng <dzh2077@gmail.com>
2022-11-26 23:37:03 -0800

    zhiheng: update coincore

--- Assets/Scripts/CoinText.cs
+++ Assets/Scripts/CoinText.cs
@@ -3,21 +3,20 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class TextInDoubleWorld : MonoBehaviour
+public class CoinText : MonoBehaviour
 {
+    // Start is called before the first frame update
     public TMP_Text text;
     private Color color;
-    // Start is called before the first frame update
     void Start()
     {
-
-        text = gameObject.GetComponent<TMP_Text>();
-    }
+        text = gameObject.GetComponent<TMP_Text>();    }
 
     // Update is called once per frame
     void Update()
     {
         color = GameObject.FindWithTag("Player").GetComponent<SpriteRenderer>().color;
-        text.color = color;
+        text.color = (color == Color.white) ? Color.black : Color.white;
+        
     }
 }
